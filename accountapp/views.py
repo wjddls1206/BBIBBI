5ea90a97ed229bf994feced1ba5696ddc091d892
dltnwjd308 <you@example.com>
2023-11-02 17:33:13 +0900

    sj push

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,8 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
 
 
 # Create your views here.
 class TestView(TemplateView):
     template_name = 'accountapp/test.html'
+
+class LogInView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    template_name = 'accountapp/login.html'
