e6a385a26505849e066fc4e2d522b44e1a50bfd0
junginny <wjddls021206@naver.com>
2023-11-21 02:50:19 +0900

    deep learning

--- BBIBBI/settings.py
+++ BBIBBI/settings.py
@@ -147,3 +147,7 @@ LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# image upload
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
